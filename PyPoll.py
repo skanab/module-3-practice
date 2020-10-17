20cad4a8110b049686617c1106092ca05eefcc15
Brian Morrison <mbrian@gmail.com>
2020-10-16 21:10:54 -0500

    from prision

--- PyPoll.py
+++ PyPoll.py
@@ -7,8 +7,15 @@
 
 
 import datetime as dt
+import os
+import csv
 
 now = dt.datetime.now()
 
 print(f'The time right now {now}')
 
+
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+with open(file_to_load) as election_data:
+    print(election_data)
