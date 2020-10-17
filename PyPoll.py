67b62fc405cd1f979a65e1c47971625e9e67e7eb
Brian Morrison <mbrian@gmail.com>
2020-10-16 23:34:11 -0500

    final 3.6

--- PyPoll.py
+++ PyPoll.py
@@ -31,30 +31,34 @@ with open(file_to_load) as election_data:
             candidate_options.append(canidate_name)
         candidate_votes[canidate_name] += 1
 
-        
-
-
-for candidate in candidate_votes.keys():
-    votes = candidate_votes[candidate]
-    vote_percentage = votes / total_votes * 100
-    print(f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
-    if votes > winning_count:
-        winning_count = votes
-        winning_candidate = candidate
-        winning_percentage = vote_percentage
-
-winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
-print(winning_candidate_summary)
-
-#Write File
 with open(file_to_save, 'w') as election_analysis:
-    election_analysis.write("Hello World!")
 
-# print(winning_candidate)
-# print(winning_count)
-# print(winning_percentage)
+    # Print the final vote count to the terminal.
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    # Save the final vote count to the text file.
+    election_analysis.write(election_results)
+
+
+    #Loop through candidates and calculate the votes and percentage for each candidate and determine the winner
+    for candidate in candidate_votes.keys():
+        votes = candidate_votes[candidate]
+        vote_percentage = votes / total_votes * 100
+        # print(f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
+        if votes > winning_count:
+            winning_count = votes
+            winning_candidate = candidate
+            winning_percentage = vote_percentage
+
+    #print win info
+    winning_candidate_summary = (
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary, end="")
+    election_analysis.write(winning_candidate_summary)
