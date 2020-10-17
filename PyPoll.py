d5a282fc19bf4a806f29793a04e695a6812594dc
Brian Morrison <mbrian@gmail.com>
2020-10-16 21:26:59 -0500

    close file

--- PyPoll.py
+++ PyPoll.py
@@ -18,3 +18,11 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 
 with open(file_to_load) as election_data:
     print(election_data)
+
+file_to_save = os.path.join("Analysis", "election_analysis.txt")
+
+outfile = open(file_to_save, "w")
+
+outfile.close()
+
+
